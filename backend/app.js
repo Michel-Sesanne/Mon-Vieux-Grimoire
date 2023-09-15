5c12f009bdebc55f02b9920547dbca9c38fcfd0a
Michel Sesanne <msesanne@gmail.com>
2023-09-15 18:01:57 +0200

    Creation d'une API RESTful

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,16 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
+const Book = require('./models/book')
 
 const app = express();
+
+mongoose.connect('mongodb+srv://michel16anne:ocrP7@cluster0.orefawd.mongodb.net/?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
 app.use(express.json());
 
 app.use((req, res, next) => {
@@ -10,45 +20,38 @@ app.use((req, res, next) => {
   next();
 });
 
-app.get('/api/books', (req, res, next) => {
-  const books = [
-    {
-      id: 'oeihfzeoi',
-      userId: 'qsomihvqios',
-      title: 'Mon premier livre',
-      author: 'Premier auteur',
-      year: 2023,
-      imageUrl: 'https://images.unsplash.com/photo-1629992101753-56d196c8aabb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1890&q=80',
-      genre: 'fx',
-      ratings: {
-        userId: 'qsomihvqios',
-        grade: 5,
-      },
-      averageRating: 3,
-    },
-    {
-      id: 'oeihfzeomoihi',
-      userId: 'qsomihvqios',
-      title: 'Mon deuxième livre',
-      author: 'Deuxième auteur',
-      year: 1900,
-      imageUrl: 'https://images.unsplash.com/photo-1544947950-fa07a98d237f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1887&q=80',
-      genre: 'roman',
-      ratings: {
-        userId: 'qsomihvqios',
-        grade: 5,
-      },
-      averageRating: 4,
-    },
-  ];
-  res.status(200).json(books);
+app.post('/api/books', (req, res, next) => {
+  delete req.body._id;
+  const book = new Book({
+    ...req.body
+  });
+  book.save()
+    .then(() => res.status(201).json({ message: 'Livre enregistré !'}))
+    .catch(error => res.status(400).json({ error }));
 });
 
-app.post('/api/books', (req, res, next) => {
-  console.log(req.body);
-  res.status(201).json({
-    message: 'Livre créé !'
-  });
+app.put('/api/books/:id', (req, res, next) => {
+  Book.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
+    .then(() => res.status(200).json({ message: 'Livre modifié !'}))
+    .catch(error => res.status(400).json({ error }));
+});
+
+app.delete('/api/books/:id', (req, res, next) => {
+  Book.deleteOne({ _id: req.params.id })
+    .then(() => res.status(200).json({ message: 'Livre supprimé !'}))
+    .catch(error => res.status(400).json({ error }));
+});
+
+app.get('/api/books/:id', (req, res, next) => {
+  Book.findOne({ _id: req.params.id })
+    .then(book => res.status(200).json(book))
+    .catch(error => res.status(404).json({ error }));
+});
+
+app.get('/api/books', (req, res, next) => {
+  Book.find()
+    .then(books => res.status(200).json(books))
+    .catch(error => res.status(400).json({ error }));
 });
 
 module.exports = app;
