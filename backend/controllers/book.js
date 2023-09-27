2ca782f96880ec48be47349670e265cadc69f200
Michel Sesanne <msesanne@gmail.com>
2023-09-27 16:03:10 +0200

    Correction bug notation

--- backend/controllers/book.js
+++ backend/controllers/book.js
@@ -7,6 +7,12 @@ exports.getAllBooks = (req, res, next) => {
     .catch(error => res.status(400).json({ error }));
 };
 
+exports.getOneBook = (req, res, next) => {
+  Book.findOne({ _id: req.params.id })
+    .then(book => res.status(200).json(book))
+    .catch(error => res.status(404).json({ error }));
+};
+
 exports.getBestRating = (req, res, next) => {
   Book.find()
       .sort({ averageRating: -1 })
@@ -15,10 +21,48 @@ exports.getBestRating = (req, res, next) => {
       .catch(error => res.status(400).json({ error }))
 };
 
-exports.getOneBook = (req, res, next) => {
+exports.deleteBook = (req, res, next) => {
+  Book.findOne({ _id: req.params.id})
+    .then(book => {
+      if (book.userId != req.auth.userId) {
+        res.status(403).json({ message: 'unauthorized request'})
+      } else {
+        const filename = book.imageUrl.split('/images/')[1];
+        fs.unlink(`images/${filename}`, () => {
+          Book.deleteOne({_id: req.params.id})
+            .then(() => { res.status(200).json({ message: 'Livre supprimé !'})})
+            .catch(error => res.status(401).json({ error }));
+        });
+      }
+    })
+    .catch(error => res.status(500).json({ error }));
+};
+
+exports.rateBook = (req, res, next) => {  
   Book.findOne({ _id: req.params.id })
-    .then(book => res.status(200).json(book))
-    .catch(error => res.status(404).json({ error }));
+      .then(book => {    
+          // Vérifier si l'utilisateur a déjà noté ce livre
+          book.ratings.map(rate => {
+              if (req.auth.userId === rate.userId) {
+                  res.status(400).json({ message: 'Vous avez déjà noté ce livre.' })
+              }
+          })
+          // Ajouter la nouvelle notation au tableau "ratings" du livre
+          book.ratings.push({
+              "userId": req.auth.userId,
+              "grade": req.body.rating
+          });
+          // Recalculer la note moyenne en utilisant toutes les notations du tableau "ratings"
+          const totalGrades = book.ratings.reduce((sum, rating) => sum + rating.grade, 0);
+          book.averageRating = totalGrades / book.ratings.length;
+          // Mettre à jour la note moyenne dans le document du livre
+          Book.updateOne({ _id: req.params.id }, book)          
+              .then(() => { res.status(201).json(book)})
+              .catch((error) => { res.status(401).json({ error }) });
+      })
+      .catch((error) => {
+          res.status(400).json({ error });
+      });
 };
 
 exports.createBook = (req, res, next) => {
@@ -61,48 +105,3 @@ exports.modifyBook = (req, res, next) => {
       })
       .catch((error) => { res.status(400).json({ error })});
 };
-
-exports.deleteBook = (req, res, next) => {
-    Book.findOne({ _id: req.params.id})
-      .then(book => {
-        if (book.userId != req.auth.userId) {
-          res.status(403).json({ message: 'unauthorized request'})
-        } else {
-          const filename = book.imageUrl.split('/images/')[1];
-          fs.unlink(`images/${filename}`, () => {
-            Book.deleteOne({_id: req.params.id})
-              .then(() => { res.status(200).json({ message: 'Livre supprimé !'})})
-              .catch(error => res.status(401).json({ error }));
-          });
-        }
-      })
-      .catch(error => res.status(500).json({ error }));
-};
-
-exports.rateBook = (req, res, next) => {
-  Book.findOne({ _id: req.params.id })
-      .then(book => {        
-          // Vérifier si l'utilisateur a déjà noté ce livre
-          book.ratings.map(rate => {
-              if (req.auth.userId === rate.userId) {
-                  res.status(400).json({ message: 'Vous avez déjà noté ce livre.' })
-              }
-          })
-          // Ajouter la nouvelle notation au tableau "ratings" du livre
-          book.ratings.push({
-              "userId": req.auth.userId,
-              "grade": req.body.rating
-          });
-          // Recalculer la note moyenne en utilisant toutes les notations du tableau "ratings"
-          const totalGrades = book.ratings.reduce((sum, rating) => sum + rating.grade, 0);
-          book.averageRating = totalGrades / book.ratings.length;
-          // Mettre à jour la note moyenne dans le document du livre
-          Book.updateOne({ _id: req.params.id }, book)
-              .then(() => { res.status(201).json({ message: 'Évaluation ajoutée avec succès', book }) })
-              .catch((error) => { res.status(401).json({ error }) });
-      })
-      .catch((error) => {
-          res.status(400).json({ error });
-      });
-};
-
